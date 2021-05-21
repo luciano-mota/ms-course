5a9f3d0470dd4d8f1ce82567214e9ae51d50c59b
Luciano Mota <amoosenhorjesus3@gmail.com>
2021-05-21 13:08:53 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -1,20 +1,10 @@
-package com.devsuperior.hrworker.entities;
+package com.devsuperior.hrpayroll.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "tb_worker")
 public class Worker implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double dailyIncome;
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -18,16 +18,16 @@ public class WorkerResource {
 
 	@Autowired
 	private WorkerRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
+	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
-	}
-	
+	}	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
-	}
+	}	
 }
